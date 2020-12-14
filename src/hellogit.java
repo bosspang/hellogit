2d33e7bc1e6e98366670d9bfc1c4a05d1780a5e8
庞春波 <60600268+bosspang@users.noreply.github.com>
2020-12-14 21:54:11 +0800

    更新2

--- src/hellogit.java
+++ src/hellogit.java
@@ -2,5 +2,6 @@ public class hellogit {
     public static void main(String[] args) {
         System.out.println("hello gitlab");
         System.out.println( "更新1");
+        System.out.println( "更新2");
     }
 }
